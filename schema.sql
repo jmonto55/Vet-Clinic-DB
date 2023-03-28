a52076d595b659e95a397e8547cd66b76ed3bcfa
Jose Montoya Gomez <jmonto55@gmail.com>
2023-03-28 12:41:26 -0500

    Add database table schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INTEGER DEFAULT 0,
+  neutered BOOLEAN DEFAULT FALSE,
+  weight_kg DECIMAL(4, 1) NOT NULL
 );
